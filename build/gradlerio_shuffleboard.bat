26cea153a54a7117a24d70299c4ef4750288bf94
Matt Yokan <matt@yokanovich.org>
2018-01-18 11:28:06 -0600

    Updates & Left/Right side support
    Updated GradleRIO and added support for allowing the close command to work for both the left and right sides by making the angle negative if the side is the right (assuming that the commands are written for the left side).
    
    May need to make changes/move side multiplier calculation to a method, will change as necessary.

---
+++ build/gradlerio_shuffleboard.bat
@@ -0,0 +1,6 @@
+@echo off
+setlocal
+pushd C:\Users\matyokanov19\.gradle\gradlerio\Shuffleboard
+java "-jar" "C:\Users\matyokanov19\.gradle\caches\modules-2\files-2.1\edu.wpi.first.shuffleboard\app\1.0.0\adc0b59e08073c0ff9c352e282118bae68e10a2a\app-1.0.0.jar"
+popd
+endlocal
--- src/main/java/frc/team2989/robot/commands/autonomous/AutoScoreCloseSwitch.java
+++ src/main/java/frc/team2989/robot/commands/autonomous/AutoScoreCloseSwitch.java
@@ -2,15 +2,18 @@ package frc.team2989.robot.commands.autonomous;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.team2989.robot.RobotMap;
+import openrio.powerup.MatchData;
 
 import java.util.stream.Stream;
 
 public class AutoScoreCloseSwitch extends CommandGroup {
 
     public AutoScoreCloseSwitch() {
+        MatchData.OwnedSide ownedSide = MatchData.getOwnedSide(MatchData.GameFeature.SWITCH_NEAR);
+        double turnAngle = RobotMap.AUTONOMOUS_CLOSE_TURN_ANGLE * ((ownedSide == MatchData.OwnedSide.LEFT) ? 1 : -1);
         Stream.of(
             new AutoDrive(RobotMap.AUTONOMOUS_CLOSE_DRIVE_DISTANCE, RobotMap.AUTONOMOUS_CLOSE_DRIVE_DEVIATION, RobotMap.AUTONOMOUS_CLOSE_DRIVE_SPEED, RobotMap.AUTONOMOUS_CLOSE_DRIVE_ROTATION),
-            new AutoTurn(RobotMap.AUTONOMOUS_CLOSE_TURN_ANGLE, RobotMap.AUTONOMOUS_CLOSE_TURN_DEVIATION, RobotMap.AUTONOMOUS_CLOSE_TURN_ROTATION, RobotMap.AUTONOMOUS_CLOSE_TURN_SPEED),
+            new AutoTurn(turnAngle, RobotMap.AUTONOMOUS_CLOSE_TURN_DEVIATION, RobotMap.AUTONOMOUS_CLOSE_TURN_ROTATION, RobotMap.AUTONOMOUS_CLOSE_TURN_SPEED),
             new AutoLaunchCube(RobotMap.AUTONOMOUS_CLOSE_LAUNCH_SPEED)
         ).forEachOrdered(this::addSequential);
     }
